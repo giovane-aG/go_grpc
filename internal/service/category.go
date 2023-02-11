4a505fc86928d91d165ea6e13aaeeaf4eee44294
Giovane <giovaneaalmeida27@gmail.com>
2023-02-11 11:58:39 -0300

    feat: create category service

---
+++ internal/service/category.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+
+	"github.com/giovane-aG/go_grpc/internal/database"
+	"github.com/giovane-aG/go_grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(db database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: db,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, input *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(input.Name, input.Description)
+	if err != nil {
+		return nil, err
+	}
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
