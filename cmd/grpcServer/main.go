a026ad911068221c7f58a7e0dc5a896e90b2c569
Giovane <giovaneaalmeida27@gmail.com>
2023-02-12 16:30:04 -0300

    feat: connects to postgres

--- cmd/grpcServer/main.go
+++ cmd/grpcServer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net"
 
 	"github.com/giovane-aG/go_grpc/internal/database"
@@ -9,10 +10,21 @@ import (
 	"github.com/giovane-aG/go_grpc/internal/service"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "./db.sqlite")
+	const PostgresDriver = "postgres"
+	const User = "postgres"
+	const Host = "localhost"
+	const Port = "5433"
+	const Password = "postgres"
+	const DbName = "postgres"
+	var DataSourceName = fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
+
+	db, err := sql.Open(PostgresDriver, DataSourceName)
 	if err != nil {
 		panic(err)
 	}
