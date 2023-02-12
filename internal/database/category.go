9cc83f9754f975cfe61f8cc8c7fa4c335b6a5a02
Giovane <giovaneaalmeida27@gmail.com>
2023-02-12 16:29:21 -0300

    fix: column name

--- internal/database/category.go
+++ internal/database/category.go
@@ -20,7 +20,7 @@ func NewCategory(db *sql.DB) *Category {
 }
 func (c *Category) Create(name, description string) (*Category, error) {
 	id := uuid.New().String()
-	_, err := c.db.Exec("INSERT INTO categories (id, name, category) VALUES ($1, $2, $3)", id, name, description)
+	_, err := c.db.Exec("INSERT INTO categories (id, name, description) VALUES ($1, $2, $3)", id, name, description)
 	if err != nil {
 		return nil, err
 	}
